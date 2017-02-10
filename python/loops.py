455922072a9edef6dad582a71d34827d0e6d19bb
Leah Brunetto <leahbrunetto@gmail.com>
2017-02-09 21:02:10 -0500

    Practiced working with loops

---
+++ python/loops.py
@@ -0,0 +1,20 @@
+# counter = 1
+# while counter <= 20:
+#     print '#' * counter
+#     counter = counter + 1
+#
+# greeting = 'Hello World'
+# for letter in greeting:
+#     print letter
+    
+# continents = ['Africa', 'Antarctica', 'Asia', 'Europe', 'North America', 'Australia', 'South America']
+# for continent in continents:
+#     print continent + ' is a continent.'
+
+
+vowels = ['a','e','i','o','u']
+def print_vowels():
+    for vowel in vowels:
+        print vowel
+
+print_vowels()
